1a84f2efe6409039fc4284df114a103b2bec11eb
Knights78 <122273242+Knights78@users.noreply.github.com>
2024-01-20 12:54:56 +0530

    Delete src directory

--- src/models/CodeExample.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const codeExampleSchema = new mongoose.Schema({
-  title: String,
-  code: String,
-  expectedOutput: String,
-});
-
-module.exports = mongoose.model('codeproblems',codeExampleSchema);
